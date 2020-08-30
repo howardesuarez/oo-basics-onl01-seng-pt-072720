7c8b98517c063d08e0d02da7ed4b30cc99bbc8dd
howard suarez <howardesuarez@gmail.com>
2020-08-30 05:02:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,7 @@
 # Make your shoe class here
 class Shoe 
-  def initialize(brand,color,size,material)
+  def initialize(brand)
     @brand=brand
-    @color=color
-    @size=size
-    @material=material
     
   end
   
